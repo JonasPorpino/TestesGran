b5960785bd0ae2adb271ad90ec5ff4f33683a9a3
JonasPorpino <49802323+JonasPorpino@users.noreply.github.com>
2024-02-05 22:44:53 -0300

    jogo da velha

--- javascript/projetosJavascript/jogoDavelha/script.js
+++ javascript/projetosJavascript/jogoDavelha/script.js
@@ -21,11 +21,12 @@ function iniciar(){
     }
 
     console.table(tabuleiro)
+    exibir()
 }
 
 // tabela dinamica
 function exibir(){
-    let tabela = '< table cellpadding="10" border="1">'
+    let tabela = '<table cellpadding="10" border="1">'
     // criar a quantidade de linhas
     
     for(let l=0; l<3; l++){
@@ -33,19 +34,77 @@ function exibir(){
         
         // criando colula
         for(let c=0; c<3; c++){
-            tabela += '<td> - </td>'
+            // conferir se deve colar x para o jogador 1 e o para o jogador 2
+            switch(tabuleiro[l][c]){
+                case -1: marcador = 'X'; break;
+                case 1: marcador = 'O'; break;
+                default: marcador = ' - ';
+            }
+            tabela += '<td>'+ marcador +'</td>'
         }
+        tabela+='</tr>'
     }
 
     tabela += '</table>'
 
     board.innerHTML = tabela
+
 }
 
+// marcar posição na tabela
 function jogar(){
+    aviso.innerHTML = 'Vez do jogador: '+ numeroJogador()
 
+    linha = document.getElementById('linha').value - 1
+    coluna = document.getElementById('coluna').value - 1
+
+    // uso do if ternario
+    if(tabuleiro[linha][coluna] == 0){
+        tabuleiro[linha][coluna] = numeroJogador() == 1 ? 1 : -1
+    } else {
+        aviso.innerHTML = 'Esse capo já foi marcado'
+
+    }
+   
+    console.table(tabuleiro)
+
+    jogador++
+    exibir()
+    checar()
 }
 
 function checar(){
 
+    // conferir se um dos jogadores ganha na linha
+    for(let i=0; i<3; i++){
+        let soma = 0
+        soma = tabuleiro[i][0] + tabuleiro[i][0] + tabuleiro[i][2]
+
+        if(soma == 3 || soma == -3){
+            aviso.innerHTML = 'O jogador '+ numeroJogador() + ' ganhou!'
+        }
+    }
+
+    // conferir se um dos jogadores ganha na coluna
+    for(let i=0; i<3; i++){
+        let soma = 0
+        soma = tabuleiro[0][i] + tabuleiro[1][i] + tabuleiro[2][i]
+
+        if(soma == 3 || soma == -3){
+            aviso.innerHTML = 'O jogador '+ numeroJogador() + ' ganhou!'
+        }
+    }
+
+    // conferir se um dos jogadores ganha na diagonal
+    soma = tabuleiro[0][0] + tabuleiro[1][1] + tabuleiro[2][2]
+
+    if(soma == 3 || soma == -3){
+        aviso.innerHTML = 'O jogador '+ numeroJogador() + ' ganhou!'
+    }
+    
+
+}
+
+function numeroJogador(){
+    return jogador%2 + 1
 }
